847c8c430dd0cb2e1a838ea19d4c700c60279dbf
Andrew Aiello <andrewaiello7@gmail.com>
2020-12-07 20:32:10 +1100

    added react-router-dom library

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="fru-header">
+            <div className="fru-logo"><p>Logo</p></div>
+            <h1>Football Round-Up</h1>
+        </header>
+    )
+}
+
+export default Header
