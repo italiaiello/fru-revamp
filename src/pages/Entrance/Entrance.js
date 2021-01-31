fa7ef63aa2e83a0307c212bf0bb6c5395decb127
Andrew Aiello <andrewaiello7@gmail.com>
2021-01-31 11:23:33 +1100

    restyled entrance screen

--- src/pages/Entrance/Entrance.js
+++ src/pages/Entrance/Entrance.js
@@ -8,17 +8,19 @@ const Entrance = () => {
     
     return (
         <section className="fru-section entrance-section">
-            <figure className="entrance-figure">
-                <img src={Fans} alt="Fans" className="responsive-img" />
-            </figure>
-            <article className="entrance-article">
-                <article id="welcome-message">
+            <article className="entrance-panel">
+                <article className="welcome-message">
                     <h2>Welcome to Football Round-Up!</h2>
-                    <h3>Stay up-to-date on the best sport in the world</h3>
+                    <p>Stay up-to-date on the best sport in the world</p>
                 </article>
+                <figure className="entrance-figure">
+                    <img src={Fans} alt="Fans" className="responsive-img" />
+                </figure>
+            </article>
+            <article className="entrance-article">
                 <button className="btn submit-button entrance-button option" onClick={() => history.push("/sign-in")}>Sign In</button>
                 <hr className="divider"></hr>
-                <p className="alternate-option" onClick={() => history.push("/leagues")}>Continue as a Guest</p>
+                <p className="alternate-option entrance-option" onClick={() => history.push("/leagues")}>Continue as a Guest</p>
             </article>
         </section>
     )
