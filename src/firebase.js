7a25c5b5df62eb3676b7302477f8d221c570b7d7
Andrew Aiello <andrewaiello7@gmail.com>
2023-01-02 12:17:24 +1100

    Refactored firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,27 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth } from 'firebase/auth';
 
+const {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_DATABASE_URL,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID
+} = process.env
+
+console.log(REACT_APP_API_KEY)
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDIJBGrKBsaFH2Fd6Qd2caIdZ4wnGT4q6I",
-  authDomain: "football-round-up-c04ff.firebaseapp.com",
-  databaseURL: "https://football-round-up-c04ff-default-rtdb.firebaseio.com",
-  projectId: "football-round-up-c04ff",
-  storageBucket: "football-round-up-c04ff.appspot.com",
-  messagingSenderId: "769746701979",
-  appId: "1:769746701979:web:48456bf27ca18a854b803e"
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DATABASE_URL,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID
 };
 
 // Initialize Firebase
